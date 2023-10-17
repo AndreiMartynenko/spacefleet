3097350c16e29ce703eeb9b377bd0fa5d01da5bf
AndrewLondon <andrewmart@mail.com>
2023-10-17 10:23:55 +0100

    DB updated

--- server/constants.go
+++ server/constants.go
@@ -1,1 +1,4 @@
 package main
+
+const ERROR_ACCESSING_DATABASE = "error accessing database"
+const WRONG_HTTP_METHOD = "wrong http method"
--- server/spacefleet_db.go
+++ server/spacefleet_db.go
@@ -4,12 +4,12 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-//     _________Spaceships_____________________________________________________
+//     _________Spacecrafts_____________________________________________________
 //     | Id      | Name 	| Class   | Crew     | Image  | Value   | Status  |
 //     | INTEGER | TEXT     | TEXT 	  | INTEGER  | TEXT   | INTEGER | TEXT    |
 
 //     _______________Armament________________
-//    | id 	    | Craft_id    | Title | Qty  |
+//    | id 	    | CraftId    | Title | Qty  |
 //    | INTEGER |  INTEGER    | TEXT  | TEXT |
 
 func createSpacecraftsTable() error {
@@ -31,4 +31,21 @@ func createSpacecraftsTable() error {
 	statement.Exec()
 	return nil
 
+}
+
+func createArmamentLogTable() error {
+	query := `CREATE TABLE IF NOT EXISTS armament (
+		Id INTEGER PRIMARY KEY AUTO_INCREMENT,
+		CraftId INTEGER,
+		Title TEXT NOT NULL,
+		Qty TEXT NOT NULL
+	)
+	`
+	statement, err := db.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer statement.Close()
+	statement.Exec()
+	return nil
 }
